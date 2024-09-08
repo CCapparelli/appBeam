ab1ca5dce28ab57c4cea8a0b81630f4824311106
CCapparelli <kdcapparelli@gmail.com>
2024-09-08 16:40:31 -0300

    Desenvolvimento
    continuação

--- src/server/catalog/admin/js/table.js
+++ src/server/catalog/admin/js/table.js
@@ -22,76 +22,83 @@ function tableCreate(el) {
     datCategories.forEach(item => {
         var tr = tbl.insertRow();
         tr.setAttribute('id', item.id);
-        tr.setAttribute('class', 'categoryRow');
+        tr.setAttribute('class', 'listRow');
 
         var td = tr.insertCell();
-        td.appendChild(getInputText(item.name, 'catNameInput'));
+        td.appendChild(document.createTextNode(item.name));
+        td.setAttribute('class', 'listName');
         td.setAttribute('colSpan', '3');
-        td.setAttribute('class', 'catName');
 
         td = tr.insertCell();
-        td.appendChild(getUpdate('update'));
+        td.appendChild(getUpdate(item, null));
 
         td = tr.insertCell();
-        td.appendChild(getCreate('create'));
+        td.appendChild(getCreate(null));
 
         td = tr.insertCell();
-        td.appendChild(getDelete('delete'));
+        td.appendChild(getDelete(item, null));
 
         const prods = datProducts.filter(x => x.catId === item.id);
         prods.forEach(prod => {
             tr = tbl.insertRow();
             tr.setAttribute('id', prod.id);
             tr.setAttribute('catId', item.id);
+            tr.setAttribute('class', 'itemRow');
 
             td = tr.insertCell();
             td.appendChild(document.createTextNode(prod.name));
-            td.setAttribute('class', 'prodName');
+            td.setAttribute('class', 'itemName');
+            td.setAttribute('class', 'tableCol');
  
             td = tr.insertCell();
             td.appendChild(document.createTextNode(prod.desc));
-            td.setAttribute('class', 'prodDesc');
+            td.setAttribute('class', 'itemDesc');
  
             td = tr.insertCell();
             td.appendChild(document.createTextNode(prod.price));
-            td.setAttribute('class', 'prodPrice');
+            td.setAttribute('class', 'itemPrice');
  
             td = tr.insertCell();
-            td.appendChild(getUpdate('update'));
+            td.appendChild(getUpdate(item, prod));
 
             td = tr.insertCell();
-            td.appendChild(getCreate('create'));
+            td.appendChild(getCreate(item));
     
             td = tr.insertCell();
-            td.appendChild(getDelete('delete'));
+            td.appendChild(getDelete(item, prod));
         });
     });
     el.appendChild(tbl);
 }
 
-function getInputText(value, className) {
-    const result = document.createElement('input');
-    result.setAttribute('class', className);
-    result.type = 'text';
-    result.value = value;
+function getCreate(parent) {
+    const parentId = parent ? parent.id : -1;
+    const msg = parentId === -1 ? 'Nova lista...' : `Novo item em ${parent.name}...`;
+    const result = document.createElement('img');
+    result.setAttribute('class', 'create');
+    result.setAttribute('title', msg);
+    result.setAttribute('onclick', `create(${parentId});`);
+    result.src = '../../../img/plus.svg';
     return result;
 }
-function getCreate(className) {
+function getUpdate(parent, child) {
+    const childId = child ? child.id : -1;
+    const msg = childId === -1 ? `Editar ${parent.name}...` : `Editar ${child.name}...`;
     const result = document.createElement('img');
-    result.setAttribute('class', className);
-    result.src = '../../img/plus.svg';
+    result.setAttribute('class', 'update');
+    result.setAttribute('title', msg);
+    result.setAttribute('onclick', `update(${parent.id},${childId});`);
+    result.src = '../../../img/edit.svg';
     return result;
 }
-function getUpdate(className) {
+function getDelete(parent, child) {
+    const childId = child ? child.id : -1;
+    const msg = childId === -1 ? `Deletar ${parent.name}...` : `Deletar ${child.name}...`;
     const result = document.createElement('img');
-    result.setAttribute('class', className);
-    result.src = '../../img/edit.svg';
-    return result;
-}
-function getDelete(className) {
-    const result = document.createElement('img');
-    result.setAttribute('class', className);
-    result.src = '../../img/minus.svg';
+    result.setAttribute('class', 'delete');
+    result.setAttribute('title', msg);
+    result.setAttribute('onclick', `delete(${parent.id},${childId});`);
+    result.src = '../../../img/minus.svg';
     return result;
 }
 
--- src/shared/js/data.js
+++ src/shared/js/data.js
@@ -2,9 +2,9 @@ const userName = 'admin';
 const userMail = 'admin@acme.com';
 
 const datCategories = [
-    {"id":"cat-1","name":"List 1"},
-    {"id":"cat-2","name":"List 2"},
-    {"id":"cat-3","name":"List 3"}
+    {"id":"list-1","name":"List 1"},
+    {"id":"list-2","name":"List 2"},
+    {"id":"list-3","name":"List 3"}
 ];
 const datProducts = [
     {"id":"item-1","catId":"cat-1","name":"Item 1","price":"100","desc":"Description"},
@@ -14,3 +14,5 @@ const datProducts = [
     {"id":"item-5","catId":"cat-3","name":"Item 5","price":"500","desc":"Description"},
     {"id":"item-6","catId":"cat-3","name":"Item 6","price":"600","desc":"Description"}
 ];
+
+var currentList, currentItem, useConnected;
