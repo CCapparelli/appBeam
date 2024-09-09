5eab5cb9fd803d7ea2e33c43d9a0993b7ae0e3aa
CCapparelli <kdcapparelli@gmail.com>
2024-09-09 05:53:00 -0300

    css

--- src/server/catalog/admin/js/table.js
+++ src/server/catalog/admin/js/table.js
@@ -1,17 +1,5 @@
 window.onload = function() {
     const root = document.getElementById('categories');
-    datCategories.forEach(item => {
-        //const divEl = document.createElement("div");
-        //divEl.setAttribute("class", "category");
-
-        //const aEl = document.createElement("a");
-        //aEl.setAttribute("class", "categoryName");
-        //aEl.innerText = item.name;
-        
-        //divEl.appendChild(aEl);
-
-        //root.appendChild(divEl);
-    });
     tableCreate(root);
 };
 
@@ -38,7 +26,7 @@ function tableCreate(el) {
         td = tr.insertCell();
         td.appendChild(getDelete(item, null));
 
-        const prods = datProducts.filter(x => x.catId === item.id);
+        const prods = datProducts.filter(x => x.listId === item.id);
         prods.forEach(prod => {
             tr = tbl.insertRow();
             tr.setAttribute('id', prod.id);
@@ -53,31 +41,36 @@ function tableCreate(el) {
             td = tr.insertCell();
             td.appendChild(document.createTextNode(prod.desc));
             td.setAttribute('class', 'itemDesc');
+            td.setAttribute('class', 'tableCol');
  
             td = tr.insertCell();
             td.appendChild(document.createTextNode(prod.price));
             td.setAttribute('class', 'itemPrice');
+            td.setAttribute('class', 'tableCol');
  
             td = tr.insertCell();
             td.appendChild(getUpdate(item, prod));
+            td.setAttribute('class', 'tableCol');
 
             td = tr.insertCell();
             td.appendChild(getCreate(item));
+            td.setAttribute('class', 'tableCol');
     
             td = tr.insertCell();
             td.appendChild(getDelete(item, prod));
+            td.setAttribute('class', 'tableCol');
         });
     });
     el.appendChild(tbl);
 }
 
 function getCreate(parent) {
-    const parentId = parent ? parent.id : -1;
-    const msg = parentId === -1 ? 'Nova lista...' : `Novo item em ${parent.name}...`;
+ //   const parentId = parent ?? parent.id;
+    const msg = (parent === null) ? 'Nova lista...' : `Novo item em ${parent.name}...`;
     const result = document.createElement('img');
     result.setAttribute('class', 'create');
     result.setAttribute('title', msg);
-    result.setAttribute('onclick', `create(${parentId});`);
+    result.setAttribute('onclick', `create("${parent}");`);
     result.src = '../../../img/plus.svg';
     return result;
 }
@@ -87,7 +80,7 @@ function getUpdate(parent, child) {
     const result = document.createElement('img');
     result.setAttribute('class', 'update');
     result.setAttribute('title', msg);
-    result.setAttribute('onclick', `update(${parent.id},${childId});`);
+    result.setAttribute('onclick', `update("${parent.id}","${childId}");`);
     result.src = '../../../img/edit.svg';
     return result;
 }
@@ -97,8 +90,19 @@ function getDelete(parent, child) {
     const result = document.createElement('img');
     result.setAttribute('class', 'delete');
     result.setAttribute('title', msg);
-    result.setAttribute('onclick', `delete(${parent.id},${childId});`);
+    result.setAttribute('onclick', `delete("${parent.id}","${childId}");`);
     result.src = '../../../img/minus.svg';
     return result;
 }
 
+function create(parent) {
+    alert(parent);
+    currentItem = null;
+    currentList = null;
+    if (parent === null) {
+        window.location.href = "parent.html";
+    } else {
+        currentList = datCategories.filter(x => x.listId === parent.id);
+        window.location.href = "child.html";
+    }
+}
--- src/shared/js/data.js
+++ src/shared/js/data.js
@@ -7,12 +7,12 @@ const datCategories = [
     {"id":"list-3","name":"List 3"}
 ];
 const datProducts = [
-    {"id":"item-1","catId":"cat-1","name":"Item 1","price":"100","desc":"Description"},
-    {"id":"item-2","catId":"cat-2","name":"Item 2","price":"200","desc":"Description"},
-    {"id":"item-3","catId":"cat-2","name":"Item 3","price":"300","desc":"Description"},
-    {"id":"item-4","catId":"cat-3","name":"Item 4","price":"400","desc":"Description"},
-    {"id":"item-5","catId":"cat-3","name":"Item 5","price":"500","desc":"Description"},
-    {"id":"item-6","catId":"cat-3","name":"Item 6","price":"600","desc":"Description"}
+    {"id":"item-1","listId":"list-1","name":"Item 1","price":"100","desc":"Description"},
+    {"id":"item-2","listId":"list-2","name":"Item 2","price":"200","desc":"Description"},
+    {"id":"item-3","listId":"list-2","name":"Item 3","price":"300","desc":"Description"},
+    {"id":"item-4","listId":"list-3","name":"Item 4","price":"400","desc":"Description"},
+    {"id":"item-5","listId":"list-3","name":"Item 5","price":"500","desc":"Description"},
+    {"id":"item-6","listId":"list-3","name":"Item 6","price":"600","desc":"Description"}
 ];
 
 var currentList, currentItem, useConnected;
